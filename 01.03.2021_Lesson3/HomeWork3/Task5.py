a9036af9f5fa2aec93866dfff11c5455a2ed61b5
notgrin <notoby69@gmail.com>
2021-03-08 18:11:17 +0200

    Task5

--- 01.03.2021_Lesson3/HomeWork3/Task5.py
+++ 01.03.2021_Lesson3/HomeWork3/Task5.py
@@ -15,11 +15,53 @@ nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
 adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
 adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
 
-
-def get_jokes():
-    print('Hello')
+import random
 
 
-print('Hi')
+def get_joke():
+    """
+    Функция возвращает шутки в виде стоки
+    :return: строку
+    """
+    noun = random.choice(nouns)
+    adverb = random.choice(adverbs)
+    adjective = random.choice(adjectives)
+    return f'{noun} {adverb} {adjective}'
 
 
+def get_jokes(n):
+    """
+    Функция возвращает шутки в виде списка строк
+    :param n: целое число шуток, которое необходимо вернуть
+    :return: список строк
+    """
+    jokes = []
+    for i in range(n):
+        jokes.append(get_joke())
+    return jokes
+
+
+print(get_jokes(n=3))
+
+
+# def get_jokes_unique(n, unique=False):
+#     nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
+#     adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
+#     adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
+#
+#     n = min(n, len(nouns), len(adverbs), len(adjectives))
+#     jokes = []
+#     for i in range(n):
+#         noun = random.choice(nouns)
+#         adverb = random.choice(adverbs)
+#         adjective = random.choice(adjectives)
+#         joke = f'{noun} {adverb} {adjective}'
+#         jokes.append(joke)
+#
+#         if unique:
+#             nouns.remove(noun)
+#             adverbs.remove(adverb)
+#             adjectives.remove(adjective)
+#     return jokes
+#
+# print(get_jokes_unique(10))
