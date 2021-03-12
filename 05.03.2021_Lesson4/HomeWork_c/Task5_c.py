36aae869c4b3fff1e7167db4a3355a0fd4bebc54
notgrin <notoby69@gmail.com>
2021-03-12 15:01:52 +0200

    Taks5c

--- 05.03.2021_Lesson4/HomeWork_c/Task5_c.py
+++ 05.03.2021_Lesson4/HomeWork_c/Task5_c.py
@@ -10,3 +10,14 @@ USD 75.18, 2020-09-05
 EUR 80.35, 2020-09-05
 """
 
+import sys
+
+from utils import get_currency_rate
+
+currencies = sys.argv[1:]
+if len(currencies) > 0:
+    for currency in currencies:
+        currency_rate, course_date = get_currency_rate(currency)
+        print(currency.upper(), currency_rate, course_date)
+else:
+    print('Передайте набор')
