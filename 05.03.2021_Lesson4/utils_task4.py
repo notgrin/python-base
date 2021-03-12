3042d9c2d21fe4a4dbcee1c6c574c121b043bfec
notgrin <notoby69@gmail.com>
2021-03-12 12:49:41 +0200

    Task4

---
+++ 05.03.2021_Lesson4/utils_task4.py
@@ -0,0 +1,46 @@
+import requests
+import requests.utils as utils
+import datetime
+
+url = 'http://www.cbr.ru/scripts/XML_daily.asp'
+response = requests.get(url)
+
+
+def get_currency_rate(*args):
+    encodings = utils.get_encoding_from_headers(response.headers)
+    content = response.content.decode(encoding=encodings)
+    for a in args:
+        start = content.find(a)
+        end = content[start:].find('</Value>')
+        currency = content[start: (start+end)]
+        first_part = currency[:3]
+        second_part = currency[-4:]
+        main_part = currency[-8: -4]
+        x = []
+        for number in main_part:
+            if number.isdigit():
+                x.extend(number)
+        full_part = ''.join(x)
+        print(f'{first_part}: {int(full_part)},{int(second_part)} руб')
+
+    time_start = content.find('ValCurs Date="')
+    time_end = content[time_start: (time_start + 24)]
+    # print(time_end)
+    time_now = []
+    for element in time_end:
+        if element.isdigit() or element == '.':
+            time_now.append(element)
+
+    date = ''.join(time_now)
+    year = date[-4:]
+    y = int(year)
+    month = date[3:5]
+    m = int(month)
+    day = date[:2]
+    d = int(day)
+
+    d1 = datetime.date(y, m, d)
+    print(f'Курсы валют актуальны до: {d1}')
+#
+#
+# get_currency_rate('USD', 'EUR', 'AUD', 'GBP')
